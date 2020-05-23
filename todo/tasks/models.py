7b5e72ab78f220817dcf3bc9c59a4055da59b322
sah_manish <manish.sah@debajyotigroup.com>
2020-05-23 15:26:24 +0545

    add field created_at and updated_at

--- todo/tasks/models.py
+++ todo/tasks/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -6,6 +7,8 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(null=True)
 
     def __str__(self):
         return self.title
